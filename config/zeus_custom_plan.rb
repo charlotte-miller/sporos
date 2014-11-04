bc2fe110e1f525cbad2e71f403c5828657191788
Chip Miller <charlespmiller@gmail.com>
2014-11-04 13:25:40 -0800

    Cleaning up TextMate env && Rails upgrade

--- config/zeus_custom_plan.rb
+++ config/zeus_custom_plan.rb
@@ -3,7 +3,9 @@ require 'zeus/rails'
 class CustomPlan < Zeus::Rails
   def test
     require 'simplecov'
-    SimpleCov.start 'rails'
+    SimpleCov.start 'rails' do
+      coverage_dir 'spec/coverage'
+    end
 
     # require all ruby files
     Dir["#{Rails.root}/lib|app/**/*.rb"].each { |f| load f }

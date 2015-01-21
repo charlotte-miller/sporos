8ea4a654f67677098a55acd484aba5a88da34b61
Chip Miller <charlespmiller@gmail.com>
2015-01-20 18:08:40 -0800

    Remove default limits for string in postgres

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,13 @@ module Sporos
     # Be sure to have the adapter's gem in your Gemfile and follow
     # the adapter's specific installation and deployment instructions.
     config.active_job.queue_adapter = :resque
+    
+    # String should use VARCHAR not VARCHAR(255)
+    initializer "postgresql.no_default_string_limit" do
+      ActiveSupport.on_load(:active_record) do
+        ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:string].delete(:limit)
+      end
+    end
   end
 end
 

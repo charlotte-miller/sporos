a9f40fc087901d3ada4a55ee7991280e0b03f893
Chip Miller <charlespmiller@gmail.com>
2015-03-31 11:46:28 -0700

    Route66 updates for unicorn

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
+custom_web: bundle exec unicorn_rails -c config/unicorn.rb -E $RAILS_ENV -D
 worker: QUEUE=* bundle exec rake resque:work
 # scheduler: bundle exec rake resque:work QUEUE=*
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,28 +1,44 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
+working_directory "#{ENV['STACK_PATH']}"
+
+listen "/tmp/web_server.sock", :backlog => 64
+
+timeout 30
+
+pid '/tmp/web_server.pid'
+
+stderr_path "#{ENV['STACK_PATH']}/log/unicorn.stderr.log"
+stdout_path "#{ENV['STACK_PATH']}/log/unicorn.stdout.log"
+
 preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+	GC.copy_on_write_friendly = true
+
+check_client_connection false
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+	old_pid = '/tmp/web_server.pid.oldbin'
+	if File.exists?(old_pid) && server.pid != old_pid
+		begin
+			Process.kill("QUIT", File.read(old_pid).to_i)
+		rescue Errno::ENOENT, Errno::ESRCH
+			# someone else did our job for us
+		end
+	end
 
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+	defined?(ActiveRecord::Base) and
+		ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+	defined?(ActiveRecord::Base) and
+		ActiveRecord::Base.establish_connection
   
   # Reset the connections to memcache-based object and session stores - this                           
   # is using a dalli-specific reset method. Otherwise you'll get wacky cache                           
   # misreads (returning incorrect values for the requested key).                                       
-  Rails.cache.reset if Rails.cache.respond_to?(:reset)                                                 
-  Rails.application.config.session_options[:cache] if Rails.application.config.session_options[:cache]
+  # Rails.cache.reset if Rails.cache.respond_to?(:reset)
+  # Rails.application.config.session_options[:cache] if Rails.application.config.session_options[:cache]
 
   # redis_url = AppConfig.redis.host
   # redis_namespace = "sporos:resque"
@@ -34,5 +50,4 @@ after_fork do |server, worker|
   #     chain.add RequestStore::SidekiqMiddleware
   #   end
   # end
-  
 end

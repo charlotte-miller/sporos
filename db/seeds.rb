b116026218f5e32e5283e594ef07d3037bd76db8
Chip Miller <charlespmiller@gmail.com>
2015-04-11 10:26:57 -0700

    Seed Questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,19 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
+# Seed Questions:
+
+  # When does the service start?
+  # What time is the service?
+  # How do I get there?
+  # Where do I park?
+  # How do I contact ___.
+  # Is there child care
+  # Is there parking
+  # Is there coffee
+  # Is there baby care
+  # Is there communion
+  # What time does the service start
+  # Where are you located
+  # Who is the pastor
+  # What denomination is the church

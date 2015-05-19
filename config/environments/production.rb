d70c0aab9265387a9249594aeb682e2480ea292f
Chip Miller <charlespmiller@gmail.com>
2015-05-19 14:32:30 -0700

    updating domain configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: AppConfig.domains.origin }
-  routes.default_url_options = { host: AppConfig.domains.origin }
+  config.action_mailer.default_url_options = { host: AppConfig.domains.primary }
+  routes.default_url_options = { host: AppConfig.domains.primary }
 end

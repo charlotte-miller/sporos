3e9486c310ee95bb57488fcdccea79ddfd02c4bc
Chip Miller <charlespmiller@gmail.com>
2015-05-29 16:25:22 -0700

    troubleshooting a loading issue w/ production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,16 +3,16 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w{ vendor.js admin.js admin.css}
-Rails.application.config.assets.precompile += ['page_initializers/*.js','community/posts/all.js']
-Rails.application.config.assets.precompile += ['library/*.js']
-# Rails.application.config.assets.precompile += ['community/**/*.js']
-
 Rails.application.config.assets.paths << Rails.root.join('app','assets','fonts')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts', 'bower_components')
 
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w{ vendor.js admin.js admin.css}
+Rails.application.config.assets.precompile += %w{ pickadate/lib/themes/default.css pickadate/lib/themes/default.date.css pickadate/lib/themes/default.time.css image-picker/image-picker/image-picker.css}
+Rails.application.config.assets.precompile += ['page_initializers/*.js','community/posts/all.js']
+Rails.application.config.assets.precompile += ['library/*.js']
+# Rails.application.config.assets.precompile += ['community/**/*.js']
 
 Rails.application.config.assets.image_optim = {pngout:false, svgo: false, pack:true, skip_missing_workers:true}
 

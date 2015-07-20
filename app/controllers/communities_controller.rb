30b062ed7f22b8d2153f715712a9396ad61c1f43
OkySabeni <okyretina@OkySabenis-MacBook-Pro.local>
2015-07-20 13:01:00 -0700

    Place featured posts to the top of the community feed.

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,18 +1,18 @@
 class CommunitiesController < ApplicationController
   # should always render index
-  
+
   before_filter :set_posts
-  
+
   def index
-    
+
   end
-  
-  def show    
+
+  def show
     render :index
   end
-  
+
 private
-  
+
   def set_posts
     @posts ||= Post.current
       .relevance_order
@@ -20,13 +20,14 @@ private
       .per(20)
       .includes(:uploaded_files, :ministry)
       .all
-    
+
+    @featured_posts = @posts.where('featured_at is not null')
   end
-  
+
   def get_ministry
     Ministry.find(community_params.id)
   end
-  
+
   def community_params
     @community_params ||= DeepStruct.new(params.permit(:id))
   end

97746e17ff0be4038c9376f7ac53c0cb14943fa4
OkySabeni <okyretina@OkySabenis-MacBook-Pro.local>
2015-07-20 13:32:40 -0700

    Add specs for featured posts

--- spec/controllers/communities_controller_spec.rb
+++ spec/controllers/communities_controller_spec.rb
@@ -1,14 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe CommunitiesController, :type => :controller do
-  
+
   describe "GET 'index'" do
-    it "returns http success" do
+    let!(:posts) { FactoryGirl.create_list(:post_event, 3, published_at: Time.now) }
+
+    before :each do
       get 'index'
+    end
+
+    it "returns http success" do
       expect(response).to be_success
     end
+
+    it 'expects published posts to be assigned' do
+      expect(assigns(:posts).to_a).to eq(posts)
+    end
+
+    describe 'featured posts' do
+      let!(:featured_posts) { [FactoryGirl.create(:post_event, published_at: Time.now, featured_at: Time.now)] }
+      it 'expects featured published posts to be assigned' do
+        expect(assigns(:featured_posts).to_a).to eq(featured_posts)
+      end
+    end
   end
-  
+
   describe "GET 'show'" do
     it "returns http success" do
       get 'show', id:'men'

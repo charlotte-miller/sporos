d6f7e2e50daeafb8c96a85b2145c4888e48eed12
Chip Miller <charlespmiller@gmail.com>
2015-08-05 11:47:49 -0700

    Revert "Skip paperclip tests for Chip." for fixing
    This reverts commit 780c5784e3e3d352ddee1f63a2da0b59db6b8c37.

--- spec/lib/paperclip_processors/upload_to_vimeo_spec.rb
+++ spec/lib/paperclip_processors/upload_to_vimeo_spec.rb
@@ -9,7 +9,6 @@ module Paperclip
     let(:lesson) { create(:lesson, {
       title: 'Message of Wonders',
       description: 'Message of wonders is a very important message.',
-      video_vimeo_id: '1',
       video_original_url:'http://foo.com/bar'}) }
     let(:run_make){ subject.make }
     let(:result)  { run_make }
@@ -23,21 +22,22 @@ module Paperclip
         run_make
       end
 
-      xit 'uploads to vimeo' do
+      it 'uploads to vimeo' do
         run_make
-        response = Typhoeus.get("https://api.vimeo.com/videos/#{@video_vimeo_id}", headers: {"Authorization" => "bearer #{AppConfig.vimeo.token}"})
+        response = Typhoeus.get("https://api.vimeo.com/videos/#{subject.vimeo_video_id}", headers: {"Authorization" => "bearer #{AppConfig.vimeo.token}"})
         expect(response.code).to eq(200)
         video_data = DeepStruct.from_json response.body
         expect(video_data.name).to eq(lesson.title)
         expect(video_data.description).to eq(lesson.description)
       end
 
-      xit 'stores the video_vimeo_id' do
+      it 'stores the video_vimeo_id' do
         run_make
-        expect(@video_vimeo_id).not_to be_nil
+        expect(subject.vimeo_video_id).not_to be_nil
+        expect(subject.vimeo_video_id).to eq(subject.vimeo_video_id)
       end
 
-      xit 'leaves video_original_url for reference' do
+      it 'leaves video_original_url for reference' do
         run_make
         expect(lesson.video_original_url).to eq('http://foo.com/bar')
         # ^ Tests an edge case where the link was stored in an attr_accessor

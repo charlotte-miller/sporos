1a7fe31ef01db18ee80fafb3a296573781d78387
Chip Miller <charlespmiller@gmail.com>
2015-08-05 16:20:52 -0700

    moving Groups helpers out of ApplicationHelpers - (all helpers are loaded at runtime) - setting up data in a before(:all)

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,25 @@
 module GroupsHelper
+  def current_meeting_from(group)
+    group.meetings.detect { |m| m.date_of > Time.now }
+  end
+
+  def current_meeting_position(meeting, current_meeting)
+    return "finished" if current_meeting.blank?
+    if meeting.position < current_meeting.position
+      "past"
+    elsif meeting.position == current_meeting.position
+      "current"
+    elsif meeting.position > current_meeting.position
+      "future"
+    end
+  end
+
+  def percent_completed(user_lesson_state, lesson)
+    if lesson.duration.present?
+      user_lesson_state.media_progress.to_f/lesson.duration.to_f * 100.0
+    else
+      0.0
+    end
+  end
+
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,32 +1,5 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
-  let(:study_group) { FactoryGirl.create(:study_group) }
-  let!(:past_meeting) { FactoryGirl.create(:meeting, date_of: Time.now - 5.minute, group: study_group) }
-  let!(:current_meeting) { FactoryGirl.create(:meeting, date_of: Time.now + 5.minute, group: study_group) }
-  let!(:future_meeting) { FactoryGirl.create(:meeting, date_of: Time.now + 15.minute, group: study_group) }
 
-  describe '#current_meeting' do
-    it { expect(current_meeting_from(study_group)).to eq(current_meeting) }
-  end
-
-  describe '#current_meeting_position' do
-    it { expect(current_meeting_position(past_meeting, current_meeting)).to eq("past") }
-    it { expect(current_meeting_position(current_meeting, current_meeting)).to eq("current") }
-    it { expect(current_meeting_position(future_meeting, current_meeting)).to eq("future") }
-
-    describe 'all meeting has finished' do
-      let(:current_meeting) { nil }
-      it { expect(current_meeting_position(future_meeting, current_meeting)).to eq("finished") }
-    end
-  end
-
-  describe '#percent_completed' do
-    let(:user_lesson_state) { FactoryGirl.build(:user_lesson_state, media_progress: 100) }
-    let(:lesson) { FactoryGirl.build(:lesson, duration: 200) }
-    let(:lesson_no_duration) { FactoryGirl.build(:lesson, duration: nil) }
-
-    it { expect(percent_completed(user_lesson_state, lesson)).to eq(50) }
-    it { expect(percent_completed(user_lesson_state, lesson_no_duration)).to eq(0.0) }
-  end
 end

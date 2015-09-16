b6f4416b71828fc371c2068eefa3cbe317f6851c
Chip Miller <charlespmiller@gmail.com>
2015-09-16 11:55:48 -0700

    Updating bower to use my fork of typeahead.js

--- vendor/assets/javascripts/bower_components/typeahead.js/src/bloodhound/search_index.js
+++ vendor/assets/javascripts/bower_components/typeahead.js/src/bloodhound/search_index.js
@@ -4,8 +4,10 @@
  * Copyright 2013-2014 Twitter, Inc. and other contributors; Licensed MIT
  */
 
-var SearchIndex = (function() {
+var SearchIndex = window.SearchIndex = (function() {
   'use strict';
+
+  var CHILDREN = 'c', IDS = 'i';
 
   // constructor
   // -----------
@@ -17,8 +19,10 @@ var SearchIndex = (function() {
       $.error('datumTokenizer and queryTokenizer are both required');
     }
 
+    this.identify = o.identify || _.stringify;
     this.datumTokenizer = o.datumTokenizer;
     this.queryTokenizer = o.queryTokenizer;
+    this.matchAnyQueryToken = o.matchAnyQueryToken;
 
     this.reset();
   }
@@ -43,7 +47,7 @@ var SearchIndex = (function() {
       _.each(data, function(datum) {
         var id, tokens;
 
-        id = that.datums.push(datum) - 1;
+        that.datums[id = that.identify(datum)] = datum;
         tokens = normalizeTokens(that.datumTokenizer(datum));
 
         _.each(tokens, function(token) {
@@ -53,14 +57,20 @@ var SearchIndex = (function() {
           chars = token.split('');
 
           while (ch = chars.shift()) {
-            node = node.children[ch] || (node.children[ch] = newNode());
-            node.ids.push(id);
+            node = node[CHILDREN][ch] || (node[CHILDREN][ch] = newNode());
+            node[IDS].push(id);
           }
         });
       });
     },
 
-    get: function get(query) {
+    get: function get(ids) {
+      var that = this;
+
+      return _.map(ids, function(id) { return that.datums[id]; });
+    },
+
+    search: function search(query) {
       var that = this, tokens, matches;
 
       tokens = normalizeTokens(this.queryTokenizer(query));
@@ -69,7 +79,7 @@ var SearchIndex = (function() {
         var node, chars, ch, ids;
 
         // previous tokens didn't share any matches
-        if (matches && matches.length === 0) {
+        if (matches && matches.length === 0 && !that.matchAnyQueryToken) {
           return false;
         }
 
@@ -77,18 +87,20 @@ var SearchIndex = (function() {
         chars = token.split('');
 
         while (node && (ch = chars.shift())) {
-          node = node.children[ch];
+          node = node[CHILDREN][ch];
         }
 
         if (node && chars.length === 0) {
-          ids = node.ids.slice(0);
+          ids = node[IDS].slice(0);
           matches = matches ? getIntersection(matches, ids) : ids;
         }
 
         // break early if we find out there are no possible matches
         else {
-          matches = [];
-          return false;
+          if (!that.matchAnyQueryToken) {
+            matches = [];
+            return false;
+          }
         }
       });
 
@@ -96,8 +108,18 @@ var SearchIndex = (function() {
         _.map(unique(matches), function(id) { return that.datums[id]; }) : [];
     },
 
+    all: function all() {
+      var values = [];
+
+      for (var key in this.datums) {
+        values.push(this.datums[key]);
+      }
+
+      return values;
+    },
+
     reset: function reset() {
-      this.datums = [];
+      this.datums = {};
       this.trie = newNode();
     },
 
@@ -122,7 +144,12 @@ var SearchIndex = (function() {
   }
 
   function newNode() {
-    return { ids: [], children: {} };
+    var node = {};
+
+    node[IDS] = [];
+    node[CHILDREN] = {};
+
+    return node;
   }
 
   function unique(array) {
@@ -141,8 +168,8 @@ var SearchIndex = (function() {
   function getIntersection(arrayA, arrayB) {
     var ai = 0, bi = 0, intersection = [];
 
-    arrayA = arrayA.sort(compare);
-    arrayB = arrayB.sort(compare);
+    arrayA = arrayA.sort();
+    arrayB = arrayB.sort();
 
     var lenArrayA = arrayA.length, lenArrayB = arrayB.length;
 
@@ -163,7 +190,5 @@ var SearchIndex = (function() {
     }
 
     return intersection;
-
-    function compare(a, b) { return a - b; }
   }
 })();

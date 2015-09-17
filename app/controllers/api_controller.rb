64391c61a844f567fa6b3db70edff749b0f4a862
Chip Miller <charlespmiller@gmail.com>
2015-09-17 14:23:38 -0700

    Moved check auth into a private method - added firebase production config

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,22 +5,24 @@ class ApiController < ActionController::Base
 
   before_filter :check_auth
 
-  def check_auth
-     authenticate_or_request_with_http_basic do |username,password|
-      resource = User.find_by_email(username)
-      if resource.valid_password?(password)
-        payload = { uid: resource.public_id, email: resource.email, name: resource.name }
-
-        generator = Firebase::FirebaseTokenGenerator.new(AppConfig.firebase.secret_access_key)
-        token = generator.create_token(payload)
-
-        render json: { token: token }, status: 200
-      else
-        render json: { message: "invalid login/password" }, status: 401
-      end
+  def login
+    if @token
+      render json: { token: @token }, status: 200
+    else
+      render json: { message: "invalid login/password" }, status: 401
     end
   end
 
-  def login
+private
+
+  def check_auth
+     authenticate_or_request_with_http_basic do |username,password|
+      resource = User.find_by_email(username)
+      if resource && resource.valid_password?(password)
+        payload = { uid: resource.public_id, email: resource.email, name: resource.name }
+        generator = Firebase::FirebaseTokenGenerator.new(AppConfig.firebase.secret_access_key)
+        @token = generator.create_token(payload)
+      end
+    end
   end
 end
